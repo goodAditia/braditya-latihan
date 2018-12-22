bcb8d14b96841a62f50d77e88cd67bc15eed3e6e
bagusAditiaSetiawan <bagus.aditia.setiwan@gmail.com>
2018-12-22 10:22:47 +0700

    firest push

---
+++ Math.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: braditya
+ * Date: 22/12/18
+ * Time: 10:12
+ */
+
+class Math
+{
+    public function plus($a, $b)
+    {
+        return $a + $b;
+    }
+}
